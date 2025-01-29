759e6da566638625e0f6132f92906b823762b3a3
marGo <ms-ka@o2.pl>
2025-01-29 18:05:08 +0100

    dobavlenie multilanguage dlya indexa

---
+++ src/translations.js
@@ -0,0 +1,181 @@
+export const translations = {
+  ru: {
+    title: "Федеральные земли и города Германии",
+    map: "КАРТА ГЕРМАНИИ",
+    weather: "ПОГОДА",
+    search: "ПОИСК",
+    about: "О НАС",
+    berlinName: "БЕРЛИН",
+    berlinDesc: "Город, где история встречается с современностью! От Бранденбургских ворот до последних трендов в искусстве и технологиях. Берлин постоянно вдохновляет! 🌆",
+    bonnName: "БОНН",
+    hamburgName: "ГАМБУРГ",
+    hamburgDesc: "Город свободы и моря! Приезжай за вдохновением и впечатлениями! Здесь каждый найдёт что-то для себя – от прогулок по Эльбе до ярких огней ночной жизни! ⛵",
+    bremenName: "БРЕМЕН",
+    brandenburgName: "БРАНДЕНБУРГ",
+    hannoverName: "ГАННОВЕР",
+    kielName: "КИЛЬ",
+    schwerinName: "ШВЕРИН",
+    schwerinDesc: "Город озёр и сказочных замков! Прогуляйся вдоль водоёмов, открой для себя историческую архитектуру и почувствуй атмосферу уюта и спокойствия. 🏰",
+    magdeburgName: "МАГДЕБУРГ",
+    potsdamName: "ПОТСДАМ",
+    potsdamDesc: "Город с духом истории и культурных традиций! Прогуляйся по старинным улочкам, открой для себя живописные площади и насладись атмосферой этого уникального города. 📖",
+    dresdenName: "ДРЕЗДЕН",
+    dresdenDesc: "Город искусства и величия! Наслаждайся потрясающими видами на Эльбу, исследуй старинные дворцы и музеи. каждый день – как путешествие во времени! 🎨",
+    erfurtName: "ЭРФУРТ",
+    wiesbadenName: "ВИСБАДЕН",
+    mainzName: "МАЙНЦ",
+    saarbruckenName: "СААРБРЮКЕН",
+    dusseldorfName: "ДЮССЕЛЬДОРФ",
+    stuttgartName: "ШТУТГАРТ",
+    munichName: "МЮНХЕН",
+    hotels: "отели"
+  },
+  de: {
+    title: "Bundesländer und Städte Deutschlands",
+    map: "KARTE VON DEUTSCHLAND",
+    weather: "WETTERKARTE",
+    search: "SUCHE",
+    about: "ÜBER UNS",
+    berlinName: "BERLIN",
+    berlinDesc: "Die Stadt, in der Geschichte auf Moderne trifft! Vom Brandenburger Tor bis hin zu modernster Kunst und Technologie – Berlin inspiriert immer wieder aufs Neue. 🌆",
+    bonnName: "BONN",
+    hamburgName: "HAMBURG",
+    hamburgDesc: "Die Stadt der Freiheit und des Meeres! Lass dich inspirieren und beeindrucken! Hier findet jeder etwas für sich – von Spaziergängen an der Elbe bis hin zu den Lichtern des Nachtlebens! ⛵",
+    bremenName: "BREMEN",
+    brandenburgName: "BRANDENBURG",
+    hannoverName: "HANNOVER",
+    kielName: "KIEL",
+    schwerinName: "SCHWERIN",
+    schwerinDesc: "Eine Stadt der Seen und märchenhaften Schlösser! Spaziere entlang der Gewässer, entdecke historische Architektur und genieße die gemütliche und ruhige Atmosphäre. 🏰",
+    magdeburgName: "MAGDEBURG",
+    potsdamName: "POTSDAM",
+    potsdamDesc: "Eine Stadt mit dem Geist von Geschichte und kulturellen Traditionen! Spaziere durch die alten Gassen, entdecke malerische Plätze und genieße die Atmosphäre dieser einzigartigen Stadt. 📖",
+    dresdenName: "DRESDEN",
+    dresdenDesc: "Eine Stadt der Kunst und der Pracht! Genieße atemberaubende Ausblicke auf die Elbe, erkunde historische Paläste und Museen. Jeder Tag ist wie eine Zeitreise! 🎨", 
+    erfurtName: "ERFURT",
+    wiesbadenName: "WIESBADEN",
+    mainzName: "MAINZ",
+    saarbruckenName: "SAARBRÜCKEN",
+    dusseldorfName: "DÜSSELDORF",
+    stuttgartName: "STUTTGART",
+    munichName: "MÜNCHEN",
+    hotels: "hotels"
+  },
+  en: {
+    title: "Federal States and Cities of Germany",
+    map: "MAP OF GERMANY",
+    weather: "WEATHER MAP",
+    search: "SEARCH",
+    about: "ABOUT US",
+    berlinName: "BERLIN",
+    berlinDesc: "The city where history meets modernity! From the Brandenburg Gate to the latest trends in art and technology, Berlin constantly inspires! 🌆",
+    bonnName: "BONN",
+    hamburgName: "HAMBURG",
+    hamburgDesc: "City of freedom and the sea! Come for inspiration and experiences! Here, everyone will find something – from walks along the Elbe to the bright lights of nightlife! ⛵",
+    bremenName: "BREMEN",
+    brandenburgName: "BRANDENBURG",
+    hannoverName: "HANNOVER",
+    kielName: "KIEL",
+    schwerinName: "SCHWERIN",
+    schwerinDesc: "A city of lakes and fairy-tale castles! Walk along the reservoirs, discover historic architecture, and feel the atmosphere of coziness and tranquility. 🏰",
+    magdeburgName: "MAGDEBURG",
+    potsdamName: "POTSDAM",
+    potsdamDesc:"A city with a spirit of history and cultural traditions! Stroll through the ancient streets, discover picturesque squares, and enjoy the atmosphere of this unique city. 📖",
+    dresdenName: "DRESDEN",
+    dresdenDesc: "A city of art and grandeur! Enjoy stunning views of the Elbe, explore ancient palaces and museums. Every day is like a journey through time! 🎨",
+    erfurtName: "ERFURT",
+    wiesbadenName: "WIESBADEN",
+    mainzName: "MAINZ",
+    saarbruckenName: "SAARBRÜCKEN",
+    dusseldorfName: "DÜSSELDORF",
+    stuttgartName: "STUTTGART",
+    munichName: "MUNICH",
+    hotels: "hotels"
+  },
+  pl: {
+    title: "Kraje związkowe i miasta Niemiec",
+    map: "MAPA NIEMIEC",
+    weather: "MAPA POGODY",
+    search: "SZUKAJ",
+    about: "O NAS",
+    berlinName: "BERLIN",
+    berlinDesc: "Miasto, gdzie historia spotyka się z nowoczesnością! Od Bramy Brandenburskiej po najnowsze trendy w sztuce i technologii. Berlin nieustannie inspiruje! 🌆",
+    bonnName: "BONN",
+    hamburgName: "HAMBURG",
+    hamburgDesc: "Miasto wolności i morza! Przyjedź po inspiracje i wrażenia! Tutaj każdy znajdzie coś dla siebie – od spacerów po Łabie po jaskrawe światła nocnego życia! ⛵",
+
+    bremenName: "BREMA",
+    brandenburgName: "BRANDENBURGIA",
+    hannoverName: "HANOWER",
+    kielName: "KILONIA",
+    schwerinName: "SZWERIN",
+    schwerinDesc: "Miasto jezior i bajkowych zamków! Przejdź się wzdłuż zbiorników wodnych, odkryj historyczną architekturę i poczuj atmosferę przytulności i spokoju. 🏰",
+    magdeburgName: "MAGDEBURG",
+    potsdamName: "POCZDAM",
+    potsdamDesc: "Miasto z duchem historii i tradycji kulturowych! Przejdź się po starych uliczkach, odkryj malownicze place i ciesz się atmosferą tego wyjątkowego miasta. 📖",
+    dresdenName: "DREZNO",
+    dresdenDesc: "Miasto sztuki i wspaniałości! Podziwiaj zapierające dech w piersiach widoki na Łabę, odkrywaj zabytkowe pałace i muzea. Każdy dzień to podróż w czasie! 🎨",
+    erfurtName: "ERFURT",
+    wiesbadenName: "WIESBADEN",
+    mainzName: "MOGUNCJA",
+    saarbruckenName: "SAARBRÜCKEN",
+    dusseldorfName: "DÜSSELDORF",
+    stuttgartName: "STUTTGART",
+    munichName: "MONACHIUM",
+    hotels: "hotele"
+  }
+};
+
+export function changeLanguage(lang) {
+  document.querySelector("h2").innerText = translations[lang].title;
+  document.querySelector(".nav a:nth-child(1)").innerText = translations[lang].map;
+  document.querySelector(".nav a:nth-child(2)").innerText = translations[lang].weather;
+  document.querySelector(".nav a:nth-child(3)").innerText = translations[lang].search;
+  document.querySelector(".nav a:nth-child(4)").innerText = translations[lang].about;
+
+  const cityNames = document.querySelectorAll(".city");
+  cityNames[0].innerText = translations[lang].berlinName;
+  cityNames[1].innerText = translations[lang].hamburgName;
+  cityNames[2].innerText = translations[lang].bremenName;
+  cityNames[3].innerText = translations[lang].hannoverName;
+  cityNames[4].innerText = translations[lang].kielName;
+  cityNames[5].innerText = translations[lang].schwerinName;
+  cityNames[6].innerText = translations[lang].magdeburgName;
+  cityNames[7].innerText = translations[lang].potsdamName;
+  cityNames[8].innerText = translations[lang].dresdenName;
+  cityNames[9].innerText = translations[lang].erfurtName;
+  cityNames[10].innerText = translations[lang].wiesbadenName;
+  cityNames[11].innerText = translations[lang].mainzName;
+  cityNames[12].innerText = translations[lang].saarbruckenName;
+  cityNames[13].innerText = translations[lang].dusseldorfName;
+  cityNames[14].innerText = translations[lang].stuttgartName;
+  cityNames[15].innerText = translations[lang].munichName;
+  cityNames[16].innerText = translations[lang].brandenburgName;
+  cityNames[17].innerText = translations[lang].bonnName;
+
+  const cityDescriptions = document.querySelectorAll(".city");
+  cityDescriptions[0].setAttribute("data-info", translations[lang].berlinDesc);
+  cityDescriptions[1].setAttribute("data-info", translations[lang].hamburgDesc);
+  cityDescriptions[2].setAttribute("data-info", translations[lang].bremenDesc);
+  cityDescriptions[3].setAttribute("data-info", translations[lang].hannoverDesc);
+  cityDescriptions[4].setAttribute("data-info", translations[lang].kielDesc);
+  cityDescriptions[5].setAttribute("data-info", translations[lang].schwerinDesc);
+  cityDescriptions[6].setAttribute("data-info", translations[lang].magdeburgDesc);
+  cityDescriptions[7].setAttribute("data-info", translations[lang].potsdamDesc);
+  cityDescriptions[8].setAttribute("data-info", translations[lang].dresdenDesc);
+  cityDescriptions[9].setAttribute("data-info", translations[lang].erfurtDesc);
+  cityDescriptions[10].setAttribute("data-info", translations[lang].wiesbadenDesc);
+  cityDescriptions[11].setAttribute("data-info", translations[lang].mainzDesc);
+  cityDescriptions[12].setAttribute("data-info", translations[lang].saarbruckenDesc);
+  cityDescriptions[13].setAttribute("data-info", translations[lang].dusseldorfDesc);
+  cityDescriptions[14].setAttribute("data-info", translations[lang].stuttgartDesc);
+  cityDescriptions[15].setAttribute("data-info", translations[lang].munichDesc);
+  cityDescriptions[15].setAttribute("data-info", translations[lang].brandenburgDesc);
+  cityDescriptions[15].setAttribute("data-info", translations[lang].bonnDesc);
+
+
+  const hotelButtons = document.querySelectorAll(".larisbtn");
+  hotelButtons.forEach(button => {
+    button.innerText = translations[lang].hotels;
+  });
+}
